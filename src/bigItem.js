edee7f40cbe30602aacb5bdc3da23ae0686d77ca
Theo Blandin <blandin.theo.44@gmail.com>
2023-08-18 13:33:15 +0100

    improve push button animation

--- src/bigItem.js
+++ src/bigItem.js
@@ -8,16 +8,32 @@ class BigItem extends React.Component {
         this.state = {
             name: '',
             icon: '',
-            animate: 0
+            animate: true,
+            push: false
         };
+        this.handleActiveButton = this.handleActiveButton.bind(this);
     }
 
     updateItem(newName, NewIcon) {
         this.setState(prevState => ({
             name: newName,
             icon: NewIcon,
-            animate: prevState.animate + 1
+            animate: true,
+            push: false
         }));
+    }
+
+    handleActiveButton() {
+        this.setState({
+            push: true,
+            animate: false
+        });
+    }
+
+    handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            this.handleActiveButton()
+        }
     }
 
     render() {
@@ -29,13 +45,17 @@ class BigItem extends React.Component {
                 marginBottom: this.props.side == "left" ? '0%' : '3%',
                 bottom: this.props.side == "right" ? 0 : 'auto',
                 right: this.props.side == "right" ? 0 : 'auto',
-                animation: this.props.side == "left" ? 'slideInFromLeft 0.4s ease-out' : 'slideInFromRight 0.4s ease-out',
+                animation: this.state.animate ? this.props.side == "left" ? 'slideInFromLeft 0.4s ease-out' : 'slideInFromRight 0.4s ease-out' : "",
             }}>
                 <div style={{
                     position: "relative",
                 }}>
-                    <button className='big-item-icon' onKeyDown={this.props.onKeyDown} onClick={this.props.onClick} style={{
-                        color: colors[this.state.icon.color]
+                    <button className='big-item-icon' onKeyDown={this.handleKeyDown} onClick={(this.handleActiveButton)} onAnimationEnd={() => {
+                        console.log("animation end");
+                        this.props.onClick()
+                    }} style={{
+                        color: colors[this.state.icon.color],
+                        animation: this.state.push ? "pushButton 300ms" : ""
                     }}>{this.state.icon.pattern}</button>
                     <div className="big-item-icon" style={{
                         backgroundColor: this.props.color,
